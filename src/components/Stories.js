a5a446f842db315d38640b7cd223bd07f024756d
vitoriacurty <vitoria.curty@hotmail.com>
2023-02-11 02:21:46 -0300

    fix structure

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,9 +1,12 @@
-import Story from "./Story"
+import { Story } from "./Story"
 
 export const Stories = () => {
   return (
     <div class="stories">
       <Story />
+      <div class="setinha">
+        <ion-icon name="chevron-forward-circle"></ion-icon>
+      </div>
     </div>
   )
 }
