522b5450ac87a2064ac42006e7576a8d661b859b
vitoriacurty <vitoria.curty@hotmail.com>
2023-02-11 02:22:10 -0300

    add map Story

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,77 +1,27 @@
 import { Usuario } from "./Usuario";
 
-export default function Story() {
+export function Story() {
+  const stories = [
+    { src: "assets/9gag.svg", usuario: "9gag" },
+    { src: "assets/meowed.svg", usuario: "meowed" },
+    { src: "assets/barked.svg", usuario: "barked" },
+    { src: "assets/nathanwpylestrangeplanet.svg", usuario: "nathanwpylestrangeplanet" },
+    { src: "assets/wawawicomics.svg", usuario: "wawawicomics" },
+    { src: "assets/respondeai.svg", usuario: "respondeai" },
+    { src: "assets/filomoderna.svg", usuario: "filomoderna" },
+    { src: "assets/memeriagourmet.svg", usuario: "memeriagourmet" }
+  ]
+
   return (
     <>
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/9gag.svg" alt="9gag" />
+      {stories.map(item => 
+        <div class="story">
+          <div class="imagem">
+            <img src={item.src} alt={item.usuario} />
+          </div>
+          <Usuario>{item.usuario}</Usuario>
         </div>
-        <Usuario>9gag</Usuario>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/meowed.svg" alt="meowed" />
-        </div>
-        <Usuario>meowed</Usuario>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/barked.svg" alt="barked" />
-        </div>
-        <Usuario>barked</Usuario>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/nathanwpylestrangeplanet.svg" alt="nathanwpylestrangeplanet" />
-        </div>
-        <Usuario>nathanwpylestrangeplanet</Usuario>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/wawawicomics.svg" alt="wawawicomics" />
-        </div>
-        <Usuario>wawawicomics</Usuario>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/respondeai.svg" alt="respondeai" />
-        </div>
-        <Usuario>respondeai</Usuario>
-
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/filomoderna.svg" alt="filomoderna" />
-        </div>
-        <Usuario>filomoderna</Usuario>
-      </div>
-
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/memeriagourmet.svg" alt="memeriagourmet" />
-        </div>
-        <Usuario>memeriagourmet</Usuario>
-      </div>
-      <Setinha />
-
-    </>
-  )
-}
-
-export function Setinha() {
-  return (
-    <>
-      <div class="setinha">
-        <ion-icon name="chevron-forward-circle"></ion-icon>
-      </div>
-      <Usuario>barked</Usuario>
+      )}
     </>
   )
 }
