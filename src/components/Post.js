54b590d61c225a1e1ea431dbe38938db5d23bf50
vitoriacurty <vitoria.curty@hotmail.com>
2023-02-11 02:42:29 -0300

    add map Post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,79 +1,51 @@
 import { Usuario } from "./Usuario"
 
 export const Post = () => {
+  const post = [
+    { srcUser: "assets/meowed.svg", user: "meowed", srcCont: "assets/gato-telefone.svg", altCont: "gato-telefone", srcCurt: "assets/respondeai.svg", altCurt: "respondeai", number: "101.523" },
+    { srcUser: "assets/barked.svg", user: "barked", srcCont: "assets/dog.svg", altCont: "dog", srcCurt: "assets/adorable_animals.svg", altCurt: "adorable_animals", number: "99.159" },
+    { srcUser: "assets/meowed.svg", user: "meowed", srcCont: "assets/gato-telefone.svg", altCont: "gato-telefone", srcCurt: "assets/respondeai.svg", altCurt: "respondeai", number: "101.523" }
+  ]
+
   return (
     <>
-      <div class="post">
-        <div class="topo">
-          <Usuario>
-            <img src="assets/meowed.svg" alt="meowed" />
-            meowed
-          </Usuario>
-          <div class="acoes">
-            <ion-icon name="ellipsis-horizontal"></ion-icon>
-          </div>
-        </div>
-
-        <div class="conteudo">
-          <img src="assets/gato-telefone.svg" alt="gato-telefone" />
-        </div>
-
-        <div class="fundo">
-          <div class="acoes">
-            <div>
-              <ion-icon name="heart-outline"></ion-icon>
-              <ion-icon name="chatbubble-outline"></ion-icon>
-              <ion-icon name="paper-plane-outline"></ion-icon>
-            </div>
-            <div>
-              <ion-icon name="bookmark-outline"></ion-icon>
+      {post.map(item =>
+        <div class="post">
+          <div class="topo">
+            <Usuario>
+              <img src={item.srcUser} alt={item.user} />
+              {item.user}
+            </Usuario>
+            <div class="acoes">
+              <ion-icon name="ellipsis-horizontal"></ion-icon>
             </div>
           </div>
 
-          <div class="curtidas">
-            <img src="assets/respondeai.svg" alt="respondeai" />
-            <div class="texto">
-              Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
+          <div class="conteudo">
+            <img src={item.srcCont} alt={item.altCont} />
+          </div>
+
+          <div class="fundo">
+            <div class="acoes">
+              <div>
+                <ion-icon name="heart-outline"></ion-icon>
+                <ion-icon name="chatbubble-outline"></ion-icon>
+                <ion-icon name="paper-plane-outline"></ion-icon>
+              </div>
+              <div>
+                <ion-icon name="bookmark-outline"></ion-icon>
+              </div>
+            </div>
+
+            <div class="curtidas">
+              <img src={item.srcCurt} alt={item.altCurt} />
+              <div class="texto">
+                Curtido por <strong>{item.altCurt}</strong> e <strong>outras {item.number} pessoas</strong>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <div class="post">
-        <div class="topo">
-          <Usuario>
-            <img src="assets/barked.svg" alt="barked" />
-            barked
-          </Usuario>
-          <div class="acoes">
-            <ion-icon name="ellipsis-horizontal"></ion-icon>
-          </div>
-        </div>
-
-        <div class="conteudo">
-          <img src="assets/dog.svg" alt="dog" />
-        </div>
-
-        <div class="fundo">
-          <div class="acoes">
-            <div>
-              <ion-icon name="heart-outline"></ion-icon>
-              <ion-icon name="chatbubble-outline"></ion-icon>
-              <ion-icon name="paper-plane-outline"></ion-icon>
-            </div>
-            <div>
-              <ion-icon name="bookmark-outline"></ion-icon>
-            </div>
-          </div>
-
-          <div class="curtidas">
-            <img src="assets/adorable_animals.svg" alt="adorable_animals" />
-            <div class="texto">
-              Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-            </div>
-          </div>
-        </div>
-      </div>
+      )}
     </>
   )
 }
