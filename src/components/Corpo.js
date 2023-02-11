7aa099dfa363c585e67a7912e758bda9200d797d
vitoriacurty <vitoria.curty@hotmail.com>
2023-02-11 00:00:58 -0300

    add in project Corpo component

---
+++ src/components/Corpo.js
@@ -0,0 +1,21 @@
+import { Stories } from "./Stories";
+import { Posts } from "./Posts";
+import { SideBar } from "./SideBar"
+
+export const Corpo = () => {
+  return (
+    <>
+      <div class="corpo">
+        <div class="esquerda">
+
+          <Stories />
+
+          <Posts />
+
+        </div>
+
+        <SideBar />
+      </div>
+    </>
+  )
+}
