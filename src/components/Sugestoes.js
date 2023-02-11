190a06401949584d4415bad0212a499ecf0f5ac0
vitoriacurty <vitoria.curty@hotmail.com>
2023-02-11 00:04:13 -0300

    add in projects Sugestoes and Sugestao components

---
+++ src/components/Sugestoes.js
@@ -0,0 +1,16 @@
+import { Sugestao } from "./Sugestao"
+
+export const Sugestoes = () => {
+  return (
+    <>
+      <div class="sugestoes">
+        <div class="titulo">
+          Sugestões para você
+          <div>Ver tudo</div>
+        </div>
+
+        <Sugestao />
+      </div>
+    </>
+  )
+}
