8dc6b97abe4842a3e0dee253890cf6a0654fd3cb
vitoriacurty <vitoria.curty@hotmail.com>
2023-02-12 21:03:13 -0300

    add shared props between components

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,9 +1,28 @@
 import { Story } from "./Story"
 
 export const Stories = () => {
+  const stories = [
+    { src: "assets/9gag.svg", usuario: "9gag" },
+    { src: "assets/meowed.svg", usuario: "meowed" },
+    { src: "assets/barked.svg", usuario: "barked" },
+    { src: "assets/nathanwpylestrangeplanet.svg", usuario: "nathanwpylestrangeplanet" },
+    { src: "assets/wawawicomics.svg", usuario: "wawawicomics" },
+    { src: "assets/respondeai.svg", usuario: "respondeai" },
+    { src: "assets/filomoderna.svg", usuario: "filomoderna" },
+    { src: "assets/memeriagourmet.svg", usuario: "memeriagourmet" }
+  ]
+
   return (
     <div class="stories">
-      <Story />
+      {stories.map(item =>
+        <Story
+          key={item.usuario}
+          src={item.src}
+          alt={item.usuario}
+          usuario={item.usuario}
+        />
+      )}
+
       <div class="setinha">
         <ion-icon name="chevron-forward-circle"></ion-icon>
       </div>
--- src/components/Story.js
+++ src/components/Story.js
@@ -1,27 +1,14 @@
 import { Usuario } from "./Usuario";
 
-export function Story() {
-  const stories = [
-    { src: "assets/9gag.svg", usuario: "9gag" },
-    { src: "assets/meowed.svg", usuario: "meowed" },
-    { src: "assets/barked.svg", usuario: "barked" },
-    { src: "assets/nathanwpylestrangeplanet.svg", usuario: "nathanwpylestrangeplanet" },
-    { src: "assets/wawawicomics.svg", usuario: "wawawicomics" },
-    { src: "assets/respondeai.svg", usuario: "respondeai" },
-    { src: "assets/filomoderna.svg", usuario: "filomoderna" },
-    { src: "assets/memeriagourmet.svg", usuario: "memeriagourmet" }
-  ]
-
+export const Story = (props) => {
   return (
     <>
-      {stories.map(item => 
-        <div class="story">
-          <div class="imagem">
-            <img src={item.src} alt={item.usuario} />
-          </div>
-          <Usuario>{item.usuario}</Usuario>
+      <div class="story">
+        <div class="imagem">
+          <img src={props.src} alt={props.usuario} />
         </div>
-      )}
+        <Usuario>{props.usuario}</Usuario>
+      </div>
     </>
   )
 }
