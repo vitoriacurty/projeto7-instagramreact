393ac7ebab29c5a71658fc662edcc46504d3c065
vitoriacurty <vitoria.curty@hotmail.com>
2023-02-12 23:27:31 -0300

    add function savePost, likePost and likeImage in onClick

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,51 +1,77 @@
+import { useState } from "react"
 import { Usuario } from "./Usuario"
 
-export const Post = () => {
-  const post = [
-    { srcUser: "assets/meowed.svg", user: "meowed", srcCont: "assets/gato-telefone.svg", altCont: "gato-telefone", srcCurt: "assets/respondeai.svg", altCurt: "respondeai", number: "101.523" },
-    { srcUser: "assets/barked.svg", user: "barked", srcCont: "assets/dog.svg", altCont: "dog", srcCurt: "assets/adorable_animals.svg", altCurt: "adorable_animals", number: "99.159" },
-    { srcUser: "assets/meowed.svg", user: "meowed", srcCont: "assets/gato-telefone.svg", altCont: "gato-telefone", srcCurt: "assets/respondeai.svg", altCurt: "respondeai", number: "101.523" }
-  ]
+export const Post = (props) => {
+  const [save, setSave] = useState("bookmark-outline")
+  const [like, setLike] = useState("heart-outline")
+  const [color, setColor] = useState("heart-outline")
+  const [curt, setCurt] = useState(props.number)
+
+  function savePost() {
+    if (save === "bookmark-outline") {
+      setSave("bookmark");
+    } else {
+      setSave("bookmark-outline");
+    }
+  }
+
+  function likePost() {
+    if (like === "heart-outline") {
+      setLike("heart");
+      setColor("red")
+      setCurt(parseFloat(curt) + 0.001)
+    } else {
+      setLike("heart-outline");
+      setColor("black")
+      setCurt(parseFloat(curt) - 0.001)
+    }
+  }
+
+  function likeImage() {
+    if (like === "heart-outline") {
+      setLike("heart");
+      setColor("red")
+      setCurt(parseFloat(curt) + 0.001)
+    }
+  }
 
   return (
     <>
-      {post.map(item =>
-        <div class="post">
-          <div class="topo">
-            <Usuario>
-              <img src={item.srcUser} alt={item.user} />
-              {item.user}
-            </Usuario>
-            <div class="acoes">
-              <ion-icon name="ellipsis-horizontal"></ion-icon>
+      <div class="post">
+        <div class="topo">
+          <Usuario>
+            <img src={props.srcUser} alt={props.user} />
+            {props.user}
+          </Usuario>
+          <div class="acoes">
+            <ion-icon name="ellipsis-horizontal"></ion-icon>
+          </div>
+        </div>
+
+        <div class="conteudo">
+          <img src={props.srcCont} alt={props.altCont} onClick={likeImage} />
+        </div>
+
+        <div class="fundo">
+          <div class="acoes">
+            <div>
+              <ion-icon class={color} name={like} onClick={likePost}></ion-icon>
+              <ion-icon name="chatbubble-outline"></ion-icon>
+              <ion-icon name="paper-plane-outline"></ion-icon>
+            </div>
+            <div>
+              <ion-icon name={save} onClick={savePost}></ion-icon>
             </div>
           </div>
 
-          <div class="conteudo">
-            <img src={item.srcCont} alt={item.altCont} />
-          </div>
-
-          <div class="fundo">
-            <div class="acoes">
-              <div>
-                <ion-icon name="heart-outline"></ion-icon>
-                <ion-icon name="chatbubble-outline"></ion-icon>
-                <ion-icon name="paper-plane-outline"></ion-icon>
-              </div>
-              <div>
-                <ion-icon name="bookmark-outline"></ion-icon>
-              </div>
-            </div>
-
-            <div class="curtidas">
-              <img src={item.srcCurt} alt={item.altCurt} />
-              <div class="texto">
-                Curtido por <strong>{item.altCurt}</strong> e <strong>outras {item.number} pessoas</strong>
-              </div>
+          <div class="curtidas">
+            <img src={props.srcCurt} alt={props.altCurt} />
+            <div class="texto">
+              Curtido por <strong>{props.altCurt}</strong> e <strong>outras {curt.toFixed(3)} pessoas</strong>
             </div>
           </div>
         </div>
-      )}
+      </div>
     </>
   )
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,28 @@
 import { Post } from "./Post"
 
 export const Posts = () => {
+  const post = [
+    { srcUser: "assets/meowed.svg", user: "meowed", srcCont: "assets/gato-telefone.svg", altCont: "gato-telefone", srcCurt: "assets/respondeai.svg", altCurt: "respondeai", number: 101.523 },
+    { srcUser: "assets/barked.svg", user: "barked", srcCont: "assets/dog.svg", altCont: "dog", srcCurt: "assets/adorable_animals.svg", altCurt: "adorable_animals", number: 99.159 },
+    { srcUser: "assets/meowed.svg", user: "meowed", srcCont: "assets/gato-telefone.svg", altCont: "gato-telefone", srcCurt: "assets/respondeai.svg", altCurt: "respondeai", number: 101.523 }
+  ]
+
   return (
-    <div class="posts">
-      <Post />
-    </div>
+    <>
+      {post.map(item =>
+        <div class="posts">
+          <Post
+            key={item.user}
+            srcUser={item.srcUser}
+            user={item.user}
+            srcCont={item.srcCont}
+            altCont={item.altCont}
+            srcCurt={item.srcCurt}
+            altCurt={item.altCurt}
+            number={item.number}
+          />
+        </div>
+      )}
+    </>
   )
 }
