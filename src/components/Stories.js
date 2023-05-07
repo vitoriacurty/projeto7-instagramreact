7eb66a9804bac3502961915844a97ea26fd3b6a9
vitoriacurty <vitoria.curty@hotmail.com>
2023-05-06 23:27:13 -0300

    add props e map no componete Stories

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,83 +1,42 @@
 export default function Stories() {
+  const stories = [
+    { usuario: "9gag", imagem: "assets/img/9gag.svg" },
+    { usuario: "meowed", imagem: "assets/img/meowed.svg" },
+    { usuario: "barked", imagem: "assets/img/barked.svg" },
+    { usuario: "nathanwpylestrangeplanet", imagem: "assets/img/nathanwpylestrangeplanet.svg" },
+    { usuario: "wawawicomics", imagem: "assets/img/wawawicomics.svg" },
+    { usuario: "respondeai", imagem: "assets/img/respondeai.svg" },
+    { usuario: "filomoderna", imagem: "assets/img/filomoderna.svg" },
+    { usuario: "memeriagourmet", imagem: "assets/img/memeriagourmet.svg" },
+  ]
   return (
     <>
       <div class="stories">
-        <div class="story">
-          <div class="imagem">
-            <img src="assets/img/9gag.svg" alt="9gag" />
-          </div>
-          <div class="usuario">
-            9gag
-          </div>
-        </div>
 
-        <div class="story">
-          <div class="imagem">
-            <img src="assets/img/meowed.svg" alt="meowed" />
-          </div>
-          <div class="usuario">
-            meowed
-          </div>
-        </div>
-
-        <div class="story">
-          <div class="imagem">
-            <img src="assets/img/barked.svg" alt="barked" />
-          </div>
-          <div class="usuario">
-            barked
-          </div>
-        </div>
-
-        <div class="story">
-          <div class="imagem">
-            <img src="assets/img/nathanwpylestrangeplanet.svg" alt="nathanwpylestrangeplanet" />
-          </div>
-          <div class="usuario">
-            nathanwpylestrangeplanet
-          </div>
-        </div>
-
-        <div class="story">
-          <div class="imagem">
-            <img src="assets/img/wawawicomics.svg" alt="wawawicomics" />
-          </div>
-          <div class="usuario">
-            wawawicomics
-          </div>
-        </div>
-
-        <div class="story">
-          <div class="imagem">
-            <img src="assets/img/respondeai.svg" alt="respondeai" />
-          </div>
-          <div class="usuario">
-            respondeai
-          </div>
-        </div>
-
-        <div class="story">
-          <div class="imagem">
-            <img src="assets/img/filomoderna.svg" alt="filomoderna" />
-          </div>
-          <div class="usuario">
-            filomoderna
-          </div>
-        </div>
-
-        <div class="story">
-          <div class="imagem">
-            <img src="assets/img/memeriagourmet.svg" alt="memeriagourmet" />
-          </div>
-          <div class="usuario">
-            memeriagourmet
-          </div>
-        </div>
+        {stories.map((s) => (
+          <Story
+            key={s.usuario}
+            usuario={s.usuario}
+            imagem={s.imagem}
+          />))}
 
         <div class="setinha">
           <ion-icon name="chevron-forward-circle"></ion-icon>
         </div>
       </div>
     </>
+  )
+}
+
+function Story(props) {
+  return (
+    <div class="story">
+      <div class="imagem">
+        <img src={props.imagem} alt={props.usuario} />
+      </div>
+      <div class="usuario">
+        {props.usuario}
+      </div>
+    </div>
   )
 }
