b3c7bdef467ab3790edf4e00c8bb6e3db101ee72
vitoriacurty <vitoria.curty@hotmail.com>
2023-05-06 23:26:47 -0300

    add props e map no componente Post

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,79 +1,88 @@
 export default function Posts() {
+  const posts = [
+    {
+      id: 1,
+      usuario: "meowed",
+      imgPerfil: "assets/img/meowed.svg",
+      imgConteudo: "assets/img/gato-telefone.svg",
+      imgCurtidas: "assets/img/respondeai.svg",
+      txtCurtidas: "respondeai",
+      likes: 101.523,
+    },
+    {
+      id: 2,
+      usuario: "barked",
+      imgPerfil: "assets/img/barked.svg",
+      imgConteudo: "assets/img/dog.svg",
+      imgCurtidas: "assets/img/adorable_animals.svg",
+      txtCurtidas: "adorable_animals",
+      likes: 99.159,
+    },
+    {
+      id: 3,
+      usuario: "meowed",
+      imgPerfil: "assets/img/meowed.svg",
+      imgConteudo: "assets/img/gato-telefone.svg",
+      imgCurtidas: "assets/img/respondeai.svg",
+      txtCurtidas: "respondeai",
+      likes: 101.523,
+    }
+  ]
   return (
     <>
       <div class="posts">
-        <div class="post">
-          <div class="topo">
-            <div class="usuario">
-              <img src="assets/img/meowed.svg" alt="meowed" />
-              meowed
-            </div>
-            <div class="acoes">
-              <ion-icon name="ellipsis-horizontal"></ion-icon>
-            </div>
+        {posts.map((p) => (
+          <Post
+            key={p.id}
+            usuario={p.usuario}
+            imgPerfil={p.imgPerfil}
+            imgConteudo={p.imgConteudo}
+            imgCurtidas={p.imgCurtidas}
+            txtCurtidas={p.txtCurtidas}
+            likes={p.likes}
+          />
+        ))}
+      </div>
+    </>
+  )
+}
+
+function Post(props) {
+  return (
+    <div class="post">
+      <div class="topo">
+        <div class="usuario">
+          <img src={props.imgPerfil} alt={props.usuario} />
+          {props.usuario}
+        </div>
+        <div class="acoes">
+          <ion-icon name="ellipsis-horizontal"></ion-icon>
+        </div>
+      </div>
+
+      <div class="conteudo">
+        <img src={props.imgConteudo} alt="gato-telefone" />
+      </div>
+
+      <div class="fundo">
+        <div class="acoes">
+          <div>
+            <ion-icon name="heart-outline"></ion-icon>
+            <ion-icon name="chatbubble-outline"></ion-icon>
+            <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
-
-          <div class="conteudo">
-            <img src="assets/img/gato-telefone.svg" alt="gato-telefone" />
-          </div>
-
-          <div class="fundo">
-            <div class="acoes">
-              <div>
-                <ion-icon name="heart-outline"></ion-icon>
-                <ion-icon name="chatbubble-outline"></ion-icon>
-                <ion-icon name="paper-plane-outline"></ion-icon>
-              </div>
-              <div>
-                <ion-icon name="bookmark-outline"></ion-icon>
-              </div>
-            </div>
-
-            <div class="curtidas">
-              <img src="assets/img/respondeai.svg" alt="respondeai" />
-              <div class="texto">
-                Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-              </div>
-            </div>
+          <div>
+            <ion-icon name="bookmark-outline"></ion-icon>
           </div>
         </div>
 
-        <div class="post">
-          <div class="topo">
-            <div class="usuario">
-              <img src="assets/img/barked.svg" alt="barked" />
-              barked
-            </div>
-            <div class="acoes">
-              <ion-icon name="ellipsis-horizontal"></ion-icon>
-            </div>
-          </div>
-
-          <div class="conteudo">
-            <img src="assets/img/dog.svg" alt="dog" />
-          </div>
-
-          <div class="fundo">
-            <div class="acoes">
-              <div>
-                <ion-icon name="heart-outline"></ion-icon>
-                <ion-icon name="chatbubble-outline"></ion-icon>
-                <ion-icon name="paper-plane-outline"></ion-icon>
-              </div>
-              <div>
-                <ion-icon name="bookmark-outline"></ion-icon>
-              </div>
-            </div>
-
-            <div class="curtidas">
-              <img src="assets/img/adorable_animals.svg" alt="adorable_animals" />
-              <div class="texto">
-                Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-              </div>
-            </div>
+        <div class="curtidas">
+          <img src={props.imgCurtidas} alt={props.txtCurtidas} />
+          <div class="texto">
+            Curtido por <strong>{props.txtCurtidas}</strong> e <strong>outras {props.likes} pessoas</strong>
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
